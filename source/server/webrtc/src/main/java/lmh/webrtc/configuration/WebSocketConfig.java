78bd9f84d967672a10d948757cf6d35030ec3f46
Hocnv0204 <nguyenhh1102@gmail.com>
2025-11-08 17:45:20 +0700

    update domain be,fe

--- source/server/webrtc/src/main/java/lmh/webrtc/configuration/WebSocketConfig.java
+++ source/server/webrtc/src/main/java/lmh/webrtc/configuration/WebSocketConfig.java
@@ -29,7 +29,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/websocket")
-                .setAllowedOriginPatterns("http://localhost:5500", "http://127.0.0.1:5500", "http://localhost:8080", "http://127.0.0.1:8080" ,"http://localhost:5173","*" )
+                .setAllowedOriginPatterns("http://localhost:5500", "http://127.0.0.1:5500", "http://localhost:8080", "http://127.0.0.1:8080" ,"http://localhost:5173","https://deploy-jnp.vercel.app" )
                 .withSockJS()
                 .setSuppressCors(true)
                 .setSessionCookieNeeded(true);
@@ -39,7 +39,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5173","https://deploy-jnp.vercel.app"));
         config.setAllowCredentials(true);
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
