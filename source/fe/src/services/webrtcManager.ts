a16a0c3f5c8813acf85d0cfbf7f3e13c2d65f172
Hocnv0204 <nguyenhh1102@gmail.com>
2025-11-09 12:46:38 +0700

    update turn server

--- source/fe/src/services/webrtcManager.ts
+++ source/fe/src/services/webrtcManager.ts
@@ -49,8 +49,12 @@ export class WebRTCManager {
   constructor(config: WebRTCConfig) {
     this.config = {
       iceServers: [
-        { urls: ["stun:stun.l.google.com:19302"] },
-        { urls: ["stun:stun1.l.google.com:19302"] },
+        { urls: "stun:stun.l.google.com:19302" },
+        {
+          urls: "turn:openrelay.metered.ca:80",
+          username: "openrelayproject",
+          credential: "openrelayproject",
+        },
       ],
       ...config,
     };
--- source/fe/src/utils/constants.ts
+++ source/fe/src/utils/constants.ts
@@ -4,6 +4,7 @@
 
 // API Base URL
 export const API_BASE_URL = "https://api-lmh-writting-practice.id.vn";
+// export const API_BASE_URL = "http://localhost:8081";
 
 // API Endpoints
 export const API_ENDPOINTS = {
